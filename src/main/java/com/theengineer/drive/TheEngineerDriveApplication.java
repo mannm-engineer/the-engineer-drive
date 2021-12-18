3bd666b6f2301727a2226a94f6b5f9db331dd658
the-engineer <mannm.engineer@gmail.com>
2021-12-18 19:01:50 +0700

    Init source code

---
+++ src/main/java/com/theengineer/drive/TheEngineerDriveApplication.java
@@ -0,0 +1,13 @@
+package com.theengineer.drive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheEngineerDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheEngineerDriveApplication.class, args);
+	}
+
+}
